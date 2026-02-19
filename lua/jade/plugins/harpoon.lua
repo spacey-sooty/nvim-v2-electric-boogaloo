c098309eaa684ed33835bb9cc36371f26a2f7c28
Jade Turner <spacey-sooty@proton.me>
2026-02-20 00:10:50 +0800

    Add .luarc.json
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- lua/jade/plugins/harpoon.lua
+++ lua/jade/plugins/harpoon.lua
@@ -5,7 +5,7 @@ return {
     config = function()
         local harpoon = require("harpoon")
         harpoon:setup()
-                
+
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<leader>;", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
@@ -14,4 +14,4 @@ return {
         vim.keymap.set("n", "<leader>j", function() harpoon:list():select(3) end)
         vim.keymap.set("n", "<leader>k", function() harpoon:list():select(4) end)
     end
-} 
+}
